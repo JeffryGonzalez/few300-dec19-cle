e6e4491ac055931f173021f9fbf63a296ced3639
Jeffry Gonzalez <jeff@hypertheory.com>
2019-12-03 13:52:16 -0500

    yep

--- src/app/features/gift-giving/reducers/index.ts
+++ src/app/features/gift-giving/reducers/index.ts
@@ -1,10 +1,19 @@
 export const featureName = 'giftGivingFeature';
+import * as fromHolidays from './holidays.reducer';
+import { ActionReducerMap } from '@ngrx/store';
 
 export interface GiftGivingState {
-
+  holidays: fromHolidays.HolidayState;
 }
 
 
-export const reducers = {
-
+export const reducers: ActionReducerMap<GiftGivingState> = {
+  holidays: fromHolidays.reducer
 };
+
+
+// SELECTORS
+
+// 1. Feature Selector
+
+// 2. Feature per branch.
